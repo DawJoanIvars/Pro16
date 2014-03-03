12f330b1b42a8fb1b007090430960a93cb9fd85e
DawJoanIvars <joan_ic_92@hotmail.com>
2014-03-03 09:45:09 +0100

    Primera parte terminada

--- proc16/src/proc16/Equipo.java
+++ proc16/src/proc16/Equipo.java
@@ -1,9 +1,64 @@
 package proc16;
 
-public class Equipo {
+import java.io.Serializable;
 
-	public Equipo() {
+public class Equipo implements Serializable{
+	//atibutos del equipo.
+	private String nombreEquipo;
+	private int golesFavor;
+	private int golesContra;
+	private int partidosGanados;
+	private int partidosPerdidos;
+	
+	//Esto es para que equipo tenga los atributos.
+	public Equipo(String nom,int golesF, int golesC, int paritdosG, int partidosP) {
 		// TODO Auto-generated constructor stub
+		nombreEquipo=nom;
+		golesFavor=golesF;
+		golesContra=golesC;
+		partidosGanados=paritdosG;
+		partidosPerdidos=partidosP;
+		//Inicializamos los objetos. 
+		nom="";
+		golesF=0;
+		golesC=0;
+		paritdosG=0;
+		partidosP=0;
 	}
-
+	
+	public void setNombre(String nombre){
+		nombreEquipo=nombre;
+	}
+	public String getNombre(){
+		return nombreEquipo;
+	}
+	
+	public void setgolesFavor(int golesF){
+		golesFavor=golesF;
+	}
+	public int getgolesFavor(){
+		return golesFavor;
+	}
+	
+	public void setgolesContra(int golesC){
+		golesContra=golesC;
+	}
+	public int getgolesContra(){
+		return golesContra;
+	}
+	
+	public void setPartidosGanados(int partidosG){
+		partidosGanados=partidosG;
+	}
+	public int getPartidosGanados(){
+		return partidosGanados;
+	}
+	
+	public void setPartidosPerdidos(int partidosP){
+		partidosPerdidos=partidosP;
+	}
+	public int getPartidosPerdidos(){
+		return partidosPerdidos;
+	}
 }
+	
--- proc16/src/proc16/VentanaEquipo.java
+++ proc16/src/proc16/VentanaEquipo.java
@@ -6,37 +6,112 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class VentanaEquipo extends JFrame {
 
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VentanaEquipo frame = new VentanaEquipo();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private JTextField textFavor;
+	private JTextField textContra;
+	private JTextField textGanados;
+	private JTextField textPerdidos;
+	private JTextField textEquipo;
+	private Equipo equipoModificar=new Equipo("",0, 0, 0, 0);
 
 	/**
 	 * Create the frame.
 	 */
 	public VentanaEquipo() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 261, 319);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JLabel equipo = new JLabel("Equipo");
+		equipo.setBounds(10, 30, 46, 14);
+		contentPane.add(equipo);
+		
+		JLabel golesFavor = new JLabel("Goles a favor");
+		golesFavor.setBounds(10, 80, 94, 14);
+		contentPane.add(golesFavor);
+		
+		JLabel golesContra = new JLabel("Goles en contra");
+		golesContra.setBounds(10, 105, 94, 14);
+		contentPane.add(golesContra);
+		
+		JLabel partidosGanados = new JLabel("Partidos ganados");
+		partidosGanados.setBounds(10, 154, 111, 14);
+		contentPane.add(partidosGanados);
+		
+		JLabel partidosPerdidos = new JLabel("Partidos perdidos");
+		partidosPerdidos.setBounds(10, 179, 111, 14);
+		contentPane.add(partidosPerdidos);
+		
+		textEquipo = new JTextField();
+		textEquipo.setBounds(82, 27, 102, 20);
+		contentPane.add(textEquipo);
+		textEquipo.setColumns(10);
+		
+		textFavor = new JTextField();
+		textFavor.setBounds(131, 74, 53, 20);
+		contentPane.add(textFavor);
+		textFavor.setColumns(10);
+		
+		textContra = new JTextField();
+		textContra.setBounds(131, 102, 53, 20);
+		contentPane.add(textContra);
+		textContra.setColumns(10);
+		
+		textGanados = new JTextField();
+		textGanados.setBounds(131, 151, 53, 20);
+		contentPane.add(textGanados);
+		textGanados.setColumns(10);
+		
+		textPerdidos = new JTextField();
+		textPerdidos.setBounds(131, 176, 53, 20);
+		contentPane.add(textPerdidos);
+		textPerdidos.setColumns(10);
+		
+		JButton guardarDatos = new JButton("Guardar Datos");
+		guardarDatos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				equipoModificar.setNombre(textEquipo.getText());
+				equipoModificar.setgolesFavor(Integer.parseInt(textFavor.getText()));
+				equipoModificar.setgolesContra(Integer.parseInt(textContra.getText()));
+				equipoModificar.setPartidosGanados(Integer.parseInt(textGanados.getText()));
+				equipoModificar.setPartidosPerdidos(Integer.parseInt(textPerdidos.getText()));
+				guardarEquipo();
+			}
+		});
+		guardarDatos.setBounds(10, 247, 117, 23);
+		contentPane.add(guardarDatos);
+	
 	}
-
+	
+	private void guardarEquipo(){
+		//
+		ObjectOutputStream salida;
+		try
+		{
+			salida= new ObjectOutputStream(new FileOutputStream("archivo.ser"));
+			salida.writeObject(equipoModificar);
+			if (salida !=null)
+				salida.close();
+		}
+		catch(IOException ioException){
+			System.err.println("Error al abrir el archivo.");
+		}
+	}
 }
