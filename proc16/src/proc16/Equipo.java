9d5db685cae6d43e1c96e086afb723943a9e50a8
DawJoanIvars <joan_ic_92@hotmail.com>
2014-03-03 10:37:39 +0100

    FInalizada la primera parte

--- proc16/src/proc16/Equipo.java
+++ proc16/src/proc16/Equipo.java
@@ -13,17 +13,20 @@ public class Equipo implements Serializable{
 	//Esto es para que equipo tenga los atributos.
 	public Equipo(String nom,int golesF, int golesC, int paritdosG, int partidosP) {
 		// TODO Auto-generated constructor stub
+		//Inicializamos los privates.
 		nombreEquipo=nom;
 		golesFavor=golesF;
 		golesContra=golesC;
 		partidosGanados=paritdosG;
 		partidosPerdidos=partidosP;
-		//Inicializamos los objetos. 
-		nom="";
-		golesF=0;
-		golesC=0;
-		paritdosG=0;
-		partidosP=0;
+	}
+	public Equipo()
+	{
+		nombreEquipo="";
+		golesFavor=0;
+		golesContra=0;
+		partidosGanados=0;
+		partidosPerdidos=0;		
 	}
 	
 	public void setNombre(String nombre){
--- proc16/src/proc16/Principal.java
+++ proc16/src/proc16/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		//Creamos una manera de pasar equipo
-		Equipo equipo=new Equipo("",0, 0, 0, 0);
+		Equipo equipo=new Equipo("vnc",0, 0, 0, 0);
 		//Aqui para abrir la ventana
 		VentanaEquipo frame = new VentanaEquipo(equipo);
 		frame.setVisible(true);
